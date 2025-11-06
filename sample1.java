f4266058eb28fa6ded3febc0d0a59de48a1cd864
mmmm <avulapriyanka10@gmail.com>
2025-11-06 14:14:10 +0530

    my first

---
+++ sample1.java
@@ -0,0 +1,11 @@
+
+class New
+{
+	public Static void main("string args[]")
+	{
+		System.out.println("hello,world");
+		System.out.println("hello,good morning");
+		System.out.println("hello,good morning");
+	}
+}
+
